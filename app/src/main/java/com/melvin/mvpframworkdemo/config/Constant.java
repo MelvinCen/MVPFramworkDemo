4c4e49ffdad59f5de660cd66a96e8971d0e9a1c1
MelvinCen <censheng00@163.com>
2017-10-06 22:39:12 +0800

    Update Constant.java

--- app/src/main/java/com/melvin/mvpframworkdemo/config/Constant.java
+++ app/src/main/java/com/melvin/mvpframworkdemo/config/Constant.java
@@ -11,5 +11,6 @@ package com.melvin.mvpframworkdemo.config;
 
 public class Constant {
     public static final int EXCEPTION_TOKEN = 102;
-    public static final String JUHE_API_KEY_WEATHER = "c68efb3ba9623794e37254addcd7647e";
+    //假的key，自己去找个接口玩
+    public static final String JUHE_API_KEY_WEATHER = "1111111";
 }
